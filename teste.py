df3b2d7b0a199db9b806ea97cd729ca9912c3a65
Tiago A. Silva <tiago@tiago.blog.br>
2023-11-07 15:32:05 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,12 @@
+# pip install selenium
+#pip install webdriver-manager
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get("https://www.g1.com.br")
